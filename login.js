f7636017b9c58d6797d3ffbe26cf8869d9da1516
bm64 <mat.bernacik@gmail.com>
2021-04-16 05:17:28 +0200

    Add sign in with google option

--- login.js
+++ login.js
@@ -59,3 +59,34 @@ function signin() {
       window.alert("Error: " + errorMessage);
     });
 }
+
+function googleLogin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  console.log(provider);
+  firebase.auth().signInWithRedirect(provider);
+  firebase
+    .auth()
+    .getRedirectResult()
+    .then((result) => {
+      if (result.credential) {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}

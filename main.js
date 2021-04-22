56e94785148157bd3f3150ae8e7ae3e1ae7ea360
bm64 <mat.bernacik@gmail.com>
2021-04-22 18:47:52 +0200

    add pwa support and favicon

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("./sw.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
+
 var currentUser = null;
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
